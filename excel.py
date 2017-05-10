62c7cd74eabd407932ebf3347734b725c276d982
Sidney Cartwright <sidney.cartwright@gmx.com>
2017-05-10 10:49:41 +0800

    Graph update.

--- excel.py
+++ excel.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 
-file = r'C:\Users\Zdenek\Dropbox\PhD\Connections.xlsx'
-data = pd.read_excel(file, sheetname='Professions', converters={'Id': str}, index_col=0)
+file = r'Connections.xlsx'
+data = pd.read_excel(file, sheetname='Professions', converters={'Id': str, 'Stages':str, 'Appointed':str}, index_col=0)
 
 
 # data['Stages'] = list(map(int, data['Stages'].split(',')))
@@ -37,6 +37,30 @@ def show_contracts():
 
     plt.show()
 
+def _is_in_stage(stage):
+    keys = []
+    for record in data.iterrows():
+        name = record[0]
+        string = record[1]['Stages']
+        stages = [int(num) for num in string.split(',')]
+        if stage in stages:
+            keys.append(name)
+    return keys
+
+def _get_attribute(attribute=None, to_integer=False):
+    atts = {}
+    for record in data.iterrows():
+        key = record[0]
+        string = record[1][attribute]
+        if string is not '-' and to_integer:
+            values = [int(num) for num in string.split(',')]
+        elif type(string) == float:
+            values = string
+        else:
+            values = string.split(',')
+
+        atts[key] = values
+    return atts
 
 def show_stage_participants(stage=0):
     '''
@@ -44,39 +68,32 @@ def show_stage_participants(stage=0):
     :return: None
     '''
     labels = {}
+    node_sizes = []
     G = nx.Graph()
 
-    for record in data.iterrows():
-        key = record[0]
-        name = record[1]['Name']
-        if type(record[1]['Stages']) == int:
-            stages = record[1]['Stages'].split(',')
-        else:
-            stages = list(record[1]['Stages'])
+    keys = _is_in_stage(stage=stage)
+    att_app = _get_attribute('Appointed')
+    att_imp = _get_attribute('Importance')
 
-        for s in stages:
-            print('s', s)
+    for key in keys:
+        G.add_node(key)
+        description = data.loc[key][0]
+        labels[key] = ''.join([description, ' ', key])
+        node_sizes.append(att_imp[key]*300)
 
-            if s in stages:
-                G.add_node(key, name=name)
-                labels[key] = ''.join([name, ' ', key])
-
-
-        for record in G.nodes():
-            print('rec', record)
-            name = record
-            appointed = data.loc[record]['Appointed']
-            print('app', appointed)
-            # if appointed is not '-':
-            #     G.add_edge(name, appointed)
+    for key in keys:
+        for item in att_app[key]:
+            if item is not '-':
+                G.add_edge(key, item)
 
     pos = nx.spring_layout(G)
-
-    nx.draw(G, pos, )
-    nx.draw_networkx_labels(G, pos, labels=labels)
+    nx.draw_networkx(G, pos, node_size=node_sizes, labels=labels, with_labels=True, alpha=0.8)
+    # nx.draw_networkx_labels(G, pos, labels=labels, node_sizes=node_sizes)
 
     plt.show()
 
 if __name__ == '__main__':
     # show_contracts()
     show_stage_participants(stage=1)
+    # ret = _is_in_stage(1)
+    # print(ret)
